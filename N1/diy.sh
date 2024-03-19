8a6040fa2a298d8d98834c6c630eae7cad87e9f2
ntwck <158731522+ntwck@users.noreply.github.com>
2024-03-19 14:35:29 +0800

    Add feed source

--- N1/diy.sh
+++ N1/diy.sh
@@ -7,5 +7,8 @@ sed -i 's/192.168.1.1/192.168.1.10/g' package/base-files/files/bin/config_genera
 rm -rf feeds/luci/applications/luci-app-passwall feeds/packages/net/v2ray-geodata
 
 # Add packages
-git clone https://github.com/nantayo/My-Pkg package/my-pkg
-git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
+#git clone https://github.com/nantayo/My-Pkg package/my-pkg
+#git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
+
+# Add feed source
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
